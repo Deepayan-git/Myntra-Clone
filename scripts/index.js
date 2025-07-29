d85d3d1ab860692a954ddf4e39d9f21732e54d7e
Deepayan-git <deepayanofficial77@gmail.com>
2025-07-29 14:38:17 +0530

    Add files via upload

---
+++ scripts/index.js
@@ -0,0 +1,68 @@
+let bagItems = [];
+onLoad()
+
+
+function onLoad() {
+  let bagItemStr = localStorage.getItem('bagItems');
+  bagItems = bagItemStr ? JSON.parse(bagItemStr) : [];
+  displayItemsOnHomePage();
+  displayBagIcon();
+
+}
+
+
+
+function addToBag(itemId) {
+  bagItems.push(itemId);
+  localStorage.setItem("bagItems", JSON.stringify(bagItems));
+  displayBagIcon();
+}
+
+function displayBagIcon() {
+  let bagItemCount = document.querySelector(".bag-item-count");
+  if (bagItems.length > 0 ){
+    bagItemCount.style.visibility = "visible";
+    bagItemCount.innerText = bagItems.length;
+  } else{
+    bagItemCount.style.visibility = "hidden";
+  }
+  
+}
+
+
+
+function displayItemsOnHomePage() {
+  let itemsContainerElement = document.querySelector(".items-container");
+  
+  if (!itemsContainerElement){
+    return;
+  }
+
+
+  let innerHtml = "";
+  items.forEach((item) => {
+    innerHtml += `
+  <div class="item-con">
+    <img class="item-image" src="${item.image}" alt="item image" >
+    <div class="rating">
+      ${item.rating.stars} ★ | ${item.rating.count}
+    </div>
+    <div class="company-name">${item.company}</div>
+    <div class="item-name">${item.item_name}</div>
+    <div class="price">
+        <span class="current-price"> ${item.current_price} </span>
+        <span class="original-price"> ${item.original_price} </span>
+        <span class="discount-price"> ( ${item.discount_percentage}  OFF) </span>
+    </div>
+    <button class="btn-add-bag"
+    onclick="addToBag(${item.id})">Add to Bag</button>
+  </div>`;
+  });
+  itemsContainerElement.innerHTML = innerHtml;
+}
+
+
+// Footer year change automatically
+const current_year = new Date().getFullYear();
+
+document.querySelector("#year").textContent = current_year;
